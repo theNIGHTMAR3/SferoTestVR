49231f6969acadc645f29ed4ff416ae33f24f381
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2023-06-08 13:54:36 +0200

    Loading and saving config file in project

--- SferoTestVR/Assets/Code/Scripts/Map/MapCreation/MapCreator.cs
+++ SferoTestVR/Assets/Code/Scripts/Map/MapCreation/MapCreator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,36 +11,51 @@ public class MapCreator : EditorWindow
     List<RoomType> rooms = new List<RoomType>();
     Vector2 scrollPos;
 
+    const string CONFIG_FILE = "config.txt";
+
+
     [MenuItem("SferoTest/Generate Map %g")]
     public static void OpenWindow()
     {
-        GetWindow<MapCreator>();
+        GetWindow<MapCreator>();        
+    }
+    
+    public void OnEnable()
+    {
+        LoadConfig();
     }
 
-
+    /// <summary>
+    /// defining window inputs and behaviour
+    /// </summary>
     void OnGUI()
     {
         GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Create New Config File"))
+        if (GUILayout.Button("Save Externaly"))
         {
-            CreateMapFile();
+            SaveExternally();
         }
 
         if (GUILayout.Button("Clear list"))
         {
             rooms.Clear();
+            SaveLocally();
         }
         GUILayout.EndHorizontal();
             
         if (GUILayout.Button("Add room"))
         {
             rooms.Add(new RoomType());
+            SaveLocally();
         }
 
         DrawRoomsList();
 
     }
 
+    /// <summary>
+    /// draw rooms in scroll view
+    /// </summary>
     void DrawRoomsList()
     {
         scrollPos = GUILayout.BeginScrollView(scrollPos, GUILayout.Height(400));
@@ -51,39 +67,84 @@ public class MapCreator : EditorWindow
         GUILayout.EndScrollView();
     }
 
+    /// <summary>
+    /// draw single room in the scroll view
+    /// returns true if element was deleted
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
     bool DrawRoom(int index)
     {
         
         GUILayout.BeginHorizontal();        
         EditorGUILayout.LabelField((index+1).ToString(), GUILayout.Width(20));
+
+        RoomType prevType = rooms[index];
         rooms[index] =(RoomType) EditorGUILayout.EnumPopup(rooms[index]);
-        if (GUILayout.Button("X"))
+
+        if (prevType != rooms[index])
         {
-            rooms.RemoveAt(index);
-            GUILayout.EndHorizontal();
-            return true;
+            SaveLocally();
+            return false;
         }
         else
         {
-            GUILayout.EndHorizontal();
-            return false;
-        }        
+            if (GUILayout.Button("X"))
+            {
+                rooms.RemoveAt(index);
+                GUILayout.EndHorizontal();
+                SaveLocally();
+                return true;
+            }
+            else
+            {
+                GUILayout.EndHorizontal();
+                return false;
+            }
+        }
     }
 
+
     /// <summary>
-    /// don't know at the moment how to preserve actual list after closing Unity
+    /// Create the config file for building and testing, in different place
     /// </summary>
-    void SaveActualSetup()
+    void SaveExternally()
     {
 
     }
-
-
     /// <summary>
-    /// Create the config file for building and testing
+    /// Saves locally the map config file. Should be called after each change
     /// </summary>
-    void CreateMapFile()
+    void SaveLocally()
     {
+        StreamWriter streamWriter = new StreamWriter(CONFIG_FILE);        
 
+        foreach(RoomType room in rooms)
+        {
+            streamWriter.WriteLine((int) room);
+        }
+
+        streamWriter.Close();
+
+    }
+
+    void LoadConfig()
+    {
+        //check if config file exist
+        if (File.Exists(CONFIG_FILE))
+        {
+            rooms = new List<RoomType>();
+            StreamReader streamReader = new StreamReader(CONFIG_FILE); //open the file
+
+            string line;
+            while ((line = streamReader.ReadLine()) != null) //read all lines
+            {
+                //add rooms to the list
+                RoomType room = (RoomType)int.Parse(line);
+                rooms.Add(room);
+            }
+
+            streamReader.Close();
+        }
     }
 }
