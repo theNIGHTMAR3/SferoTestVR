310097ae2746178efbd4f16edb76b05e55628f55
Michał Kuprianowicz <michal.kuprianowicz@interia.pl>
2023-06-26 03:11:58 +0200

    Gate and simple FPSPlayer created

---
+++ SferoTestVR/Assets/Code/Scripts/Map/GateTriggerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateTriggerController : MonoBehaviour
+{
+    [SerializeField] private bool isOpened = false;
+
+    [SerializeField] private Animator myGate = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            if (isOpened)
+            {
+                myGate.Play("GateClose", 0, 0.0f);
+                isOpened = false;
+            }
+            else
+            {
+                myGate.Play("GateOpen", 0, 0.0f);
+                isOpened = true;
+            }
+        }
+    }
+}
