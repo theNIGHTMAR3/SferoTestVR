baa25744304d17f7e3867dd01c152d311e2cb1c6
Michał Kuprianowicz <michal.kuprianowicz@interia.pl>
2023-07-14 13:21:58 +0200

    Fixed backend of PlayerPC

--- SferoTestVR/Assets/Code/Scripts/Player/CameraFollower.cs
+++ SferoTestVR/Assets/Code/Scripts/Player/CameraFollower.cs
@@ -5,14 +5,12 @@ using static UnityEngine.GraphicsBuffer;
 
 public class CameraFollower : MonoBehaviour
 {
-    public GameObject player;
-
     public float sensitivity = 5f;
 
+    private GameObject player;
+   
     private Vector2 mouseRotation;
     
-    public float xOffset, yOffset, zOffset;
-
 
     // Start is called before the first frame update
     void Start()
@@ -25,20 +23,11 @@ public class CameraFollower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // FPP camera
-        if (xOffset == 0 && yOffset== 0 && zOffset == 0)
-        {
-            transform.position = player.transform.position;
+        transform.position = player.transform.position;
 
-            mouseRotation.x += Input.GetAxis("Mouse X") * sensitivity;
-            mouseRotation.y += Input.GetAxis("Mouse Y") * sensitivity;
-            transform.localRotation = Quaternion.Euler(-mouseRotation.y, mouseRotation.x, 0);    
-        }
-        // TPP camera
-        else
-        {
-            transform.position = player.transform.position + new Vector3(xOffset, yOffset, zOffset);
-            transform.LookAt(player.transform.position);
-        }
+        mouseRotation.x += Input.GetAxis("Mouse X") * sensitivity;
+        mouseRotation.y += Input.GetAxis("Mouse Y") * sensitivity;
+        transform.localRotation = Quaternion.Euler(-mouseRotation.y, mouseRotation.x, 0);    
+     
     }
 }
--- SferoTestVR/Assets/Code/Scripts/Player/PlayerPCController.cs
+++ SferoTestVR/Assets/Code/Scripts/Player/PlayerPCController.cs
@@ -2,56 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerPCController : MonoBehaviour
+public class PlayerPCController : Player
 {
-    public GameObject playerObject;
-
-    public float moveSpeed = 10f;
-    public float sensitivity = 5f;
-
-    private Rigidbody playerRigidbody;
-    private Camera playerCamera;
-
-    private float xInput;
-    private float zInput;
-
-    private Vector2 mouseRotation;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerObject = GameObject.FindGameObjectWithTag("Player");
-        playerCamera = playerObject.GetComponentInChildren<Camera>(); 
-        playerRigidbody= playerObject.GetComponentInChildren<Rigidbody>(); 
-
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        xInput = Input.GetAxis("Horizontal");
-        zInput = Input.GetAxis("Vertical");
-
-        playerCamera.transform.position = playerRigidbody.transform.position;
-
-        mouseRotation.x += Input.GetAxis("Mouse X") * sensitivity;
-        mouseRotation.y += Input.GetAxis("Mouse Y") * sensitivity;
-        playerCamera.transform.localRotation = Quaternion.Euler(-mouseRotation.y, mouseRotation.x, 0);
-    }
 
     private void FixedUpdate()
     {
-        Vector3 cameraForward= playerCamera.transform.forward;
+        Vector3 cameraForward= Camera.main.transform.forward;
 
         cameraForward.y = 0f;
         cameraForward.Normalize();
 
-        Vector3 playerMovement = (cameraForward * zInput + playerCamera.transform.right * xInput) * moveSpeed;
+        Vector3 playerMovement = (cameraForward * playerInput.y + Camera.main.transform.right * playerInput.x) * moveSpeed;
 
-
-        playerRigidbody.AddForce(playerMovement);
+        Move(playerMovement);
     }
+
+
+    protected override void GetInput()
+    {
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+    }
+
 }
