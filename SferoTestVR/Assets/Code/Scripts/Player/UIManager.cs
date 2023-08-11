c14148038ab8aa3afe7d0c2dabf3e7314d367c40
Michał Kuprianowicz <michal.kuprianowicz@interia.pl>
2023-08-11 21:56:09 +0200

    Added death screen
    main menu preparation

---
+++ SferoTestVR/Assets/Code/Scripts/Player/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance;
+    public GameObject deathUI;
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<UIManager>();
+            }
+            return instance;
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        deathUI.SetActive(false);
+    }
+
+    public void ShowDeathUI()
+    {
+        deathUI.SetActive(true);
+    }
+
+    public void HideDeathUI()
+    {
+        deathUI.SetActive(false);
+    }
+}
