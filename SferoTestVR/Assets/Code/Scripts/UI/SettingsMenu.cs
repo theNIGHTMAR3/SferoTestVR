63d519eb325a9d2eb6906c4d2d7e2a1568313c32
Michał Kuprianowicz <michal.kuprianowicz@interia.pl>
2023-08-20 18:32:31 +0200

    Basic MainMenu
    Features:
    - start the game
    - open settings menu
    - quit the game

---
+++ SferoTestVR/Assets/Code/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public void SetSensitivity(float sentivity)
+    {
+        Debug.Log("Changed sensitivity to " + sentivity);
+    }
+
+    public void SetSphereDiameter(float diameter)
+    {
+        Debug.Log("Changed sphere diameter to " + diameter);
+    }
+
+}
