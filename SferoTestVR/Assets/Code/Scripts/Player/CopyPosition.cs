58e8fb28dfcee20bf7293ca74cd14d00fa5a6dac
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2023-08-25 21:47:15 +0200

    Player VR Home

---
+++ SferoTestVR/Assets/Code/Scripts/Player/CopyPosition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyPosition : MonoBehaviour
+{
+    [SerializeField] GameObject gameObject;
+    [SerializeField] Vector3 offset = Vector3.zero;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = gameObject.transform.position + offset;
+    }
+}
