5fc53e69669f0c6a0993c152ca129374034dcfab
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2023-10-11 21:04:20 +0200

    Build Editor Fix
    Moved editor classess to Editor directory. Otherwise build isn't possible

--- SferoTestVR/Assets/Code/Scripts/Obstacles/ActionQueue/Action.cs
+++ SferoTestVR/Assets/Code/Scripts/Obstacles/ActionQueue/Action.cs
@@ -93,39 +93,3 @@ public class Action
     }
 
 }
-
-[CustomEditor(typeof(Action))]
-public class ActionEditor : Editor
-{
-    SerializedProperty type;
-    SerializedProperty newPos;
-    SerializedProperty newRot;
-    SerializedProperty newScale;    
-
-    void OnEnable()
-    {
-        type = serializedObject.FindProperty("type");
-        newPos = serializedObject.FindProperty("newPos");
-        newRot = serializedObject.FindProperty("newRot");
-        newScale = serializedObject.FindProperty("newScale");
-    }
-
-    public override void OnInspectorGUI()
-    {
-        serializedObject.Update();
-        EditorGUILayout.PropertyField(type);
-        EditorGUILayout.PropertyField(newPos);
-        EditorGUILayout.PropertyField(newRot);
-        EditorGUILayout.PropertyField(newScale);
-
-        serializedObject.ApplyModifiedProperties();
-        if (type.enumValueIndex == (int)Action.ActionType.MOVE)
-        {
-            EditorGUILayout.LabelField("MOVE");
-        }
-        else
-        {
-            EditorGUILayout.LabelField("NOT MOVE");
-        }
-    }
-}
---
+++ SferoTestVR/Assets/Editor/ActionEditor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Action))]
+public class ActionEditor : Editor
+{
+    SerializedProperty type;
+    SerializedProperty newPos;
+    SerializedProperty newRot;
+    SerializedProperty newScale;
+
+    void OnEnable()
+    {
+        type = serializedObject.FindProperty("type");
+        newPos = serializedObject.FindProperty("newPos");
+        newRot = serializedObject.FindProperty("newRot");
+        newScale = serializedObject.FindProperty("newScale");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(type);
+        EditorGUILayout.PropertyField(newPos);
+        EditorGUILayout.PropertyField(newRot);
+        EditorGUILayout.PropertyField(newScale);
+
+        serializedObject.ApplyModifiedProperties();
+        if (type.enumValueIndex == (int)Action.ActionType.MOVE)
+        {
+            EditorGUILayout.LabelField("MOVE");
+        }
+        else
+        {
+            EditorGUILayout.LabelField("NOT MOVE");
+        }
+    }
+}
