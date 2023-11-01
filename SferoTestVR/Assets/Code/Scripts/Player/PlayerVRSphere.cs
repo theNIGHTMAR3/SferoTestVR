29c19daa1e505a51b656c11b29aaa037ce9d9d03
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2023-11-01 17:35:50 +0100

    First try of adding VirtuSphere dll
    First iteration of adding this dll.
    There is single error, however it still allows running the game. WHY?
    erro: Unable to resolve referene 'DotNetty.Transport'
    
    SpherePlayer should connect to the sphere, and convert real movement to ingame movement

--- SferoTestVR/Assets/Code/Scripts/Player/PlayerVRSphere.cs
+++ SferoTestVR/Assets/Code/Scripts/Player/PlayerVRSphere.cs
@@ -1,15 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VirtuSphereClient;
+using VirtuSphereClient.Events;
 
 public class PlayerVRSphere : Player
 {
 
+    [SerializeField] float speedMultiplier = 1;
+    [SerializeField] string hostIP="127.0.0.1";
+    [SerializeField] int hostPort = 4445;
+
+    private VirtuSphere virtuSphere;
+
+    //input from
+    SpherePoseEvent sphereInput;
+
+
+    protected override void Start()
+    {
+        base.Start();
+
+
+        virtuSphere = new VirtuSphere(VirtuSphere.ClientMode.CM_CONTROL_MOTORS);
+        virtuSphere.onConnected += onConnected;
+        virtuSphere.onDisconnected += onDisconnected;
+
+        virtuSphere.registerEventListener<SpherePoseEvent>(onSpherePoseEvent);
+
+        Debug.Log("CONNECTING");
+        virtuSphere.connect(hostIP, hostPort);
+
+    }
+
     /// <summary>
-    /// gets input from the sphere
+    /// function to analyze input from the sphere. Called in update!
     /// </summary>
     protected override void GetInput()
     {
-        //TODO
+        if (sphereInput != null)
+        {
+            //get sphere veloticy direction
+            Vector2 vel = new Vector2(
+                sphereInput.getVelocityVectorX(),
+                sphereInput.getVelocityVectorY()
+                );
+
+            //MAY NEED SOME ADJUSTMENTS f.e.: multiplier or even rotation
+            vel *= speedMultiplier;
+
+            //simple rotate the player the same way
+            Move(vel);
+        }
+    }
+
+    /// <summary>
+    /// Function for async getting input from the sphere
+    /// </summary>
+    /// <param name="evt"></param>
+    private void onSpherePoseEvent(SpherePoseEvent evt)
+    {
+        sphereInput = evt;
+    }
+
+
+    private void onConnected()
+    {
+        Debug.Log("SPHERE CONNECTED");
+        virtuSphere.requestSpherePoseUpdates(20);
+    }
+
+    private void onDisconnected()
+    {
+        Debug.Log("SPHERE DISCONNECTED");
     }
 }
