8378d5d6ae7030b7bceff19f01e2c4e639be2dc8
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2023-11-01 19:41:23 +0100

    Comments to remove error

--- SferoTestVR/Assets/Code/Scripts/Player/PlayerVRSphere.cs
+++ SferoTestVR/Assets/Code/Scripts/Player/PlayerVRSphere.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using VirtuSphereClient;
-using VirtuSphereClient.Events;
+//using VirtuSphereClient;
+//using VirtuSphereClient.Events;
 
 public class PlayerVRSphere : Player
 {
@@ -11,17 +11,17 @@ public class PlayerVRSphere : Player
     [SerializeField] string hostIP="127.0.0.1";
     [SerializeField] int hostPort = 4445;
 
-    private VirtuSphere virtuSphere;
+    //private VirtuSphere virtuSphere;
 
     //input from
-    SpherePoseEvent sphereInput;
+    //SpherePoseEvent sphereInput;
 
 
     protected override void Start()
     {
         base.Start();
 
-
+         /*
         virtuSphere = new VirtuSphere(VirtuSphere.ClientMode.CM_CONTROL_MOTORS);
         virtuSphere.onConnected += onConnected;
         virtuSphere.onDisconnected += onDisconnected;
@@ -30,7 +30,7 @@ public class PlayerVRSphere : Player
 
         Debug.Log("CONNECTING");
         virtuSphere.connect(hostIP, hostPort);
-
+         */
     }
 
     /// <summary>
@@ -38,22 +38,25 @@ public class PlayerVRSphere : Player
     /// </summary>
     protected override void GetInput()
     {
-        if (sphereInput != null)
+        //if (sphereInput != null)
         {
             //get sphere veloticy direction
+            /*
             Vector2 vel = new Vector2(
                 sphereInput.getVelocityVectorX(),
                 sphereInput.getVelocityVectorY()
                 );
-
+            
             //MAY NEED SOME ADJUSTMENTS f.e.: multiplier or even rotation
             vel *= speedMultiplier;
 
             //simple rotate the player the same way
             Move(vel);
+            */
         }
     }
 
+    /*
     /// <summary>
     /// Function for async getting input from the sphere
     /// </summary>
@@ -74,4 +77,5 @@ public class PlayerVRSphere : Player
     {
         Debug.Log("SPHERE DISCONNECTED");
     }
+    */
 }
