9482568ce1ad4fc4588b582939a14b8e6aab28be
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2023-11-01 21:45:45 +0100

    2 display mode
    -addedd script for initializing second display
    -it's put in the vr players
    -ui moved to vr players display
    -player vrhome put in main menu (to prevent sickness while game is being configured)

---
+++ SferoTestVR/Assets/Code/Scripts/Player/InitilalizeVRCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitilalizeVRCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+#if UNITY_EDITOR
+#else
+        Display.displays[1].Activate();
+#endif
+    }
+
+}
