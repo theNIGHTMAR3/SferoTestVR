a87c576826ea0c49f13e8b61d3b93984054845e3
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2023-11-07 19:08:59 +0100

    Build preview fix

--- SferoTestVR/Assets/Editor/BuildModificator.cs
+++ SferoTestVR/Assets/Editor/BuildModificator.cs
@@ -7,10 +7,11 @@ using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 
-public class BuildModificator : IPostprocessBuildWithReport
+public class BuildModificator : IPreprocessBuildWithReport
 {
     public int callbackOrder { get { return 0; } }    
-    public void OnPostprocessBuild(BuildReport report)
+   
+    public void OnPreprocessBuild(BuildReport report)
     {
         #region copy config file
         string folder = Path.GetDirectoryName(report.summary.outputPath);                
