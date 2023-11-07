02b38f4fde4c5b2d977dfab7b8a9f75dfbeb8883
Michał Kuprianowicz <michal.kuprianowicz@interia.pl>
2023-11-07 23:22:46 +0100

    SFER-60: added audio files and SoundManager

---
+++ SferoTestVR/Assets/Code/Scripts/Obstacles/Lava.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+	[SerializeField] private AudioClip lavaSizzle;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			SoundManager.instance.PlaySound(lavaSizzle);
+		}
+
+	}
+
+}
