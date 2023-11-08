d11ce58e7034dc0fe8f430f731f9a48050c09ff2
Michał Kuprianowicz <michal.kuprianowicz@interia.pl>
2023-11-08 22:29:21 +0100

    SFER-60: added basic sound logic
    - spikes sound
    - pusher with spikes sound
    -

---
+++ SferoTestVR/Assets/Code/Scripts/Obstacles/PlaySoundOnPlayerCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnPlayerCollision : MonoBehaviour
+{
+
+	[SerializeField] private AudioClip audioClip;
+	[SerializeField] private AudioSource audioSource;
+	[SerializeField] private bool playOnPlayer;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			if(playOnPlayer)
+			{
+				SoundManager.instance.PlaySoundOnPlayer(audioClip);
+			}
+			else
+			{
+				audioSource.PlayOneShot(audioClip);
+			}
+			
+		}
+
+	}
+
+}
