317337f2bba7de7c71ce75b6c9d47bbdd8b88fc8
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-03-10 19:26:08 +0100

    SFER-90: prevent instant snapping

--- SferoTestVR/Assets/Code/Scripts/ControlZone/CylinderZone.cs
+++ SferoTestVR/Assets/Code/Scripts/ControlZone/CylinderZone.cs
@@ -28,26 +28,30 @@ public class CylinderZone : ControlZone
     private void Update()
     {
         cylinderObject.transform.localRotation *= Quaternion.Euler(0,rotationAngleSpeed*Time.deltaTime,0);
-        
     }
 
     protected override void MoveToTarget()
     {
-        Vector3 cylinderInfluence = right * cylinderSpeed;
-        Vector3 direction = (endPoint.transform.position - startPoint.transform.position).normalized;
-        
+        if (targetPoint == startPoint)
+        {
+            base.MoveToTarget();
+        }
+        else
+        {
+            Vector3 cylinderInfluence = right * cylinderSpeed;
+            Vector3 direction = (endPoint.transform.position - startPoint.transform.position).normalized;
 
-        Vector3 sum = cylinderInfluence + (direction * (moveSpeed - cylinderInfluence.magnitude));        
-        
-        Vector3 offsetFromStart = CastPointOnLine(
-                startPoint.transform.position,
-                endPoint.transform.position,
-                playerController.transform.position
-            );
-        playerRb.MovePosition(startPoint.transform.position + offsetFromStart);
-        playerController.Move(sum.normalized, moveSpeed);
-        //playerRb.velocity = sum.normalized * moveSpeed;
-        //playerRb.angularVelocity = sum.normalized * moveSpeed;
+
+            Vector3 sum = cylinderInfluence + (direction * (moveSpeed - cylinderInfluence.magnitude));
+
+            Vector3 offsetFromStart = CastPointOnLine(
+                    startPoint.transform.position,
+                    endPoint.transform.position,
+                    playerController.transform.position
+                );
+            playerRb.MovePosition(startPoint.transform.position + offsetFromStart);
+            playerController.Move(sum.normalized, moveSpeed);
+        }
     }
 
 
