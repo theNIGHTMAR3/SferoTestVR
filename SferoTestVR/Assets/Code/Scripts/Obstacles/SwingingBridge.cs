94937f150d2bf9557c9a9f43aa11deb611ecffd3
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-03-30 19:30:31 +0200

    SFER-101: Make more elements in the bridge

--- SferoTestVR/Assets/Code/Scripts/Obstacles/SwingingBridge.cs
+++ SferoTestVR/Assets/Code/Scripts/Obstacles/SwingingBridge.cs
@@ -8,20 +8,45 @@ public class SwingingBridge : MonoBehaviour
     [SerializeField] float swingAngle = 45f;
     [SerializeField] float swingFrequency = 0.5f;
 
+    [SerializeField] Transform bridgeElementsParent;
+
+
     Quaternion startRot;
+    float bridgeLength = 0;
+    Vector3 bridgeNormal;
 
     private void Start()
     {
-        startRot = transform.localRotation; 
+        startRot = transform.localRotation;
+        var firstElement = bridgeElementsParent.GetChild(0);
+        var lastElement = bridgeElementsParent.GetChild(bridgeElementsParent.childCount - 1);
+        bridgeLength = Vector3.Distance(
+            firstElement.localPosition, // first
+            lastElement.localPosition // last
+            );
+
+        bridgeNormal = lastElement.localPosition - firstElement.localPosition;     
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localRotation = startRot * Quaternion.Euler(
+        var firstElement = bridgeElementsParent.GetChild(0);
+
+        foreach (Transform element in bridgeElementsParent)
+        {
+            var projected = Vector3.Project(
+                element.localPosition - firstElement.localPosition
+                , bridgeNormal
+                );
+            var t = projected.magnitude / bridgeLength;
+            var intensity = t <= 0.5f ? 2*t : 2*(1 - t);
+
+            element.localRotation = startRot * Quaternion.Euler(
             0,
             0,
-            Mathf.Sin(Time.time * swingFrequency) * swingAngle
+            Mathf.Sin(Time.time * swingFrequency) * swingAngle * intensity
             );
+        }
     }
 }
