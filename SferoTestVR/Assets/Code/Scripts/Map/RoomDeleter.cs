05cf7057090d358d4bd7aa6f907b1e3e8c395c95
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-04-09 00:22:21 +0200

    SFER-118: Room deleter script

---
+++ SferoTestVR/Assets/Code/Scripts/Map/RoomDeleter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomDeleter : MonoBehaviour
+{
+    private bool deleted = false;
+
+    private void Start()
+    {
+        var roomParent = transform.GetComponentInParent<Room>();
+        if (roomParent == null)
+        {
+            Destroy(this);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!deleted && other.gameObject.CompareTag("Player"))
+        {
+            MapLoader.Instance.DeleteRoom();
+        }
+    }
+}
