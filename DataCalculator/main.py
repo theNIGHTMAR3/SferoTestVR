5d3efd761db44d6df873a2d5b7d248d23d83230f
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-09-22 10:06:09 +0200

    Draw walk speed graphs

--- DataCalculator/main.py
+++ DataCalculator/main.py
@@ -1,6 +1,11 @@
 import os
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 
 from Walk import WalkSequence, WalkCharacteristics
+
+VELOCITY_DELTA = 0.1
 
 TRACKS_FOLDER = "Tracks"
 def load_all_walk_sequences() -> list[WalkSequence]:
@@ -10,6 +15,21 @@ def load_all_walk_sequences() -> list[WalkSequence]:
         walks.append(WalkSequence(walk_sequence_directory))
     return walks
 
+
+def plot_velocities(vels:list[float], title: str) -> None:
+    bins = np.arange(0, np.max(vels), VELOCITY_DELTA)
+    counts, edges = np.histogram(vels, bins=bins)
+    # X values: midpoints of bins
+    x = (edges[:-1] + edges[1:]) / 2
+
+    # Plot
+    plt.gca().xaxis.set_major_locator(MultipleLocator(0.5))
+    plt.bar(edges[:-1], counts, width=VELOCITY_DELTA, align="edge", edgecolor="black")
+    plt.xlabel("Prędkość sfery rzeczywistej [m/s]")
+    plt.ylabel("Liczba pomiarów")
+    plt.title(title)
+    plt.grid(True)
+    plt.show()
 
 
 sequences = load_all_walk_sequences()
@@ -27,3 +47,7 @@ for sequence in sequences:
 total_walk_characteristic.calculate()
 
 print(total_walk_characteristic)
+
+plot_velocities(total_walk_characteristic.speed_outside_zone, "Histogram prędkości sfery poza strefą kontroli")
+plot_velocities(total_walk_characteristic.speed_in_zone, "Histogram prędkości sfery w strefie kontroli")
+
